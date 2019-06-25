62ac1d4758dd6cc5af0242c43c77472e626fb8ca
古城大地 <donkey@kojoudaichinoMacBook-Pro.local>
2019-06-25 19:45:25 +0900

    0

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get "posts/index" => "posts#index"
+  # ここに投稿詳細ページへのルーティングを追加してください
+  get "posts/:id" => "posts#show"
+
+  get "/" => "home#top"
+  get "about" => "home#about"
+end
